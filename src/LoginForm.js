e245a6b0136eaf2be6cd79fb33035f25d57a50b2
RosaS22 <salcer2284@gmail.com>
2024-08-11 21:12:49 -0600

    added comments to project

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,23 +1,30 @@
 import React from 'react';
 
-
+// Functional component for the login form
 function LoginForm() {
-    return (
-        <div className="login-form">
-            <h3>Log In</h3>
-            <form>
+  return (
+    <div className="login-form">
+      {/* Header for the login form */}
+      <h3>Log In</h3>
+
+      {/* Simple form with username and password fields */}
+      <form>
         <div>
-            <label>Username: </label>
-            <input type="text" name="username" />
+          {/* Label and input for the username */}
+          <label>Username: </label>
+          <input type="text" name="username" />
         </div>
         <div>
-            <label>Password: </label>
-            <input type="password" name="password" />
+          {/* Label and input for the password */}
+          <label>Password: </label>
+          <input type="password" name="password" />
         </div>
+        {/* Submit button for the form */}
         <button type="submit">Submit</button>
-        </form>
-        </div>
-    );
+      </form>
+    </div>
+  );
 }
 
+// Export the component for use in other parts of the app
 export default LoginForm;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-
+// Functional component for the navigation bar
 function Navigation() {
   return (
     <nav className="navbar">
+      {/* Unordered list to structure the navigation links */}
       <ul>
+        {/* Placeholder links for the navigation bar */}
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
@@ -13,4 +15,5 @@ function Navigation() {
   );
 }
 
+// Export the component to be used in the main application
 export default Navigation;
